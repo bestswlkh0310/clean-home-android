439aaeef2dcd8c615de77f7cdd4646f281861d79
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 13:14:15 +0900

    feat : add complete api

--- app/src/main/java/com/bestswlkh0310/clean_home/service/api/ItemApi.kt
+++ app/src/main/java/com/bestswlkh0310/clean_home/service/api/ItemApi.kt
@@ -1,11 +1,17 @@
 package com.bestswlkh0310.clean_home.service.api
 
 import com.bestswlkh0310.clean_home.model.ItemModel
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ItemApi {
 
-    @GET("/all")
+    @GET("/item/all")
     suspend fun getAll(): ArrayList<ItemModel>
 
+    @POST("/item/complete")
+    suspend fun complete(
+        @Body itemModel: ItemModel
+    ): ArrayList<ItemModel>
 }
