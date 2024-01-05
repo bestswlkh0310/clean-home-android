c87b7aca9ed58388ceadc58d6540eba4ea0bfecf
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 11:03:14 +0900

    design : top app bar color

--- app/src/main/java/com/bestswlkh0310/clean_home/ui/my/MyView.kt
+++ app/src/main/java/com/bestswlkh0310/clean_home/ui/my/MyView.kt
@@ -9,9 +9,12 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -33,6 +36,10 @@ fun MyView(
     Scaffold(
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White,
+                    titleContentColor = Color.Black
+                ),
                 title = {
                     Text(
                         text = "MY",
