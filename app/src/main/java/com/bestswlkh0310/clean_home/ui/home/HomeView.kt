66f285d53b8873583eeebd246bebc3adbb9aec16
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 21:03:56 +0900

    fix : refresh range

--- app/src/main/java/com/bestswlkh0310/clean_home/ui/home/HomeView.kt
+++ app/src/main/java/com/bestswlkh0310/clean_home/ui/home/HomeView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -131,7 +132,8 @@ fun HomeView(
         ) {
             LazyColumn(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 if (items.isEmpty() && !isLoading) {
